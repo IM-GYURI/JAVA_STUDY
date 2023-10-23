a3a83df06a065ee8a87bc48c00477b2f9541221d
IM-GYURI <kgulr017@gmail.com>
2023-10-23 23:08:48 +0900

    자바 스터디 ch19

---
+++ Chapter19/src/InsertSort.java
@@ -0,0 +1,35 @@
+
+public class InsertSort
+{
+	public static void insertSort(int[] num)
+	{
+		// 삽입 정렬 로직 구현
+		int size = num.length;
+		int temp = 0;
+		int j = 0;
+		
+		for (int i = 1; i < size; i++)
+		{
+			temp = num[i];
+			for(j = i - 1; j >= 0 && temp < num[j]; j--)
+			{
+				num[j+1] = num[j];
+			}
+			num[j+1] = temp;
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		int num[] = {5, 3, 1, 4, 2};
+		insertSort(num);
+		
+		// 출력 로직 구현
+		for(int i = 0; i < num.length; i++)
+		{
+			System.out.print(num[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
